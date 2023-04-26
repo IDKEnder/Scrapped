af8940083080e3c071e1b45c6e358bff1fe2bb1f
IDKEnder <130079171+IDKEnder@users.noreply.github.com>
2023-04-26 21:33:39 +0800

    26/04/23 - Rng Bottles
    Added RNG to bottles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int Bottle = 0;
+    public int DoubleBottle = 0;
+    public int TripleBottle = 0;
+    public int GoldenBottle = 0;
+    public int BrokenBottle = 0;
     public int RawPlasticMaterial = 0;
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
-    public GameObject Bottle;
+    public GameObject[] OtherObjects;
     public float delay = 2f;
+    public float[] spawnRates; // The probability of spawning each other object
+    public int numObjectsToPool = 5; // The number of objects to pool from
+
     public void SpawnObject()
     {
         StartCoroutine(SpawnObjectWithDelay());
@@ -14,9 +17,39 @@ public class Spawner : MonoBehaviour
 
     IEnumerator SpawnObjectWithDelay()
     {
-        Destroy(Instantiate(ObjectToSpawn, transform.position, transform.rotation), delay);
+        // Spawn ObjectToSpawn and destroy it after delay
+        GameObject obj = Instantiate(ObjectToSpawn, transform.position, transform.rotation);
+        Destroy(obj, delay);
+
         yield return new WaitForSeconds(delay);
-        GameObject Bottlee = Instantiate(Bottle, transform.position, transform.rotation);
-        Bottlee.AddComponent<Bottle>();
+
+        // Choose which object to spawn based on spawnRates
+        float totalSpawnRate = 0f;
+        for (int i = 0; i < spawnRates.Length; i++)
+        {
+            totalSpawnRate += spawnRates[i];
+        }
+
+        float randomValue = Random.Range(0f, totalSpawnRate);
+
+        GameObject objectToSpawn = null;
+        for (int i = 0; i < spawnRates.Length; i++)
+        {
+            randomValue -= spawnRates[i];
+            if (randomValue <= 0f)
+            {
+                objectToSpawn = OtherObjects[i];
+                break;
+            }
+        }
+
+        if (objectToSpawn == null)
+        {
+            // If ObjectToSpawn is null, default to the first object in the pool
+            objectToSpawn = OtherObjects[0];
+        }
+
+        // Spawn the chosen object
+        Instantiate(objectToSpawn, transform.position, transform.rotation);
     }
 }
